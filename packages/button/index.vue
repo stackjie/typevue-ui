ec2d7fdf87cd86d5c471e37662bc612152044d73
stackjie <stackjie@gmail.com>
2018-03-13 11:53:10 +0800

    init

---
+++ packages/button/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <a href="javascript:;" class="weui-btn weui-btn_primary"></a>
+</template>
+
+<script>
+  export default {
+    name: 'vam-button'
+  }
+</script>
+
+<style lang="less">
+  @import "../weui/widget/weui-button/weui-button";
+</style>
